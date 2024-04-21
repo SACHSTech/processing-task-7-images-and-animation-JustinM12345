85a1fe38ec6290bb12c4a520bff98117c3607bb8
Justin Mui <justin.mui25@ycdsbk12.ca>
2024-04-20 22:11:40 -0400

    Done

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,61 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+	PImage imgSpaceBackground;
+  PImage imgUFO;
+  
+  float fltUFOX = 150;
+  float fltUFOY = 150;
+  float fltUFOSpeedX = 3;
+  float fltUFOSpeedY = 3;
+  float fltUFOAngle = 0.05f;
+
+  float fltCircleX = 400;
+  float fltCircleY = 400;
+  float fltCircleSpeed = 2;
+
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    imgSpaceBackground = loadImage("images/spaceBackground.jpeg");
+    imgSpaceBackground.resize(width, height);
+
+    imgUFO = loadImage("images/spaceUFO.png");
+    imgUFO.resize(width / 5, height / 5);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+	// Space Background Image
+    background(imgSpaceBackground);
+
+  // UFO Image Moving
+    image(imgUFO, fltUFOX, fltUFOY);
+
+    fltUFOX = cos(fltUFOAngle) * 50 + 189;
+    fltUFOY = sin(fltUFOAngle) * 50 + 111;
+
+    fltUFOAngle -= 0.05f;
+    fltUFOX += fltUFOSpeedX;
+    fltUFOY += fltUFOSpeedY;
+
+  // Circle Moving
+    ellipse(fltCircleX, fltCircleY, 80, 80);
+    fltCircleX = fltCircleX + fltCircleSpeed;
+
+    if(fltCircleX <= 0){
+      fltCircleSpeed = 2;
+    }
+
+    else if(fltCircleX >= width - 30){
+      fltCircleSpeed = -2;
+    }
+
   }
   
-  // define other methods down here.
 }
